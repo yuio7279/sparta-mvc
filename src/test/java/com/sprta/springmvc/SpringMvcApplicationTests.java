9f78cce2ff4df68d8a110ff2708ec8031db92be9
yuio7279 <yuio7279@gmail.com>
2023-06-12 16:40:05 +0900

    init commit

---
+++ src/test/java/com/sprta/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprta.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
